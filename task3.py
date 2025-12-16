0af9644477cdb778f354d867dd1bd94280331a8d
owenrichards557-alt <owen.richards557@students.cmcss.net>
2025-12-16 11:38:50 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,2 +1,63 @@
 #AFTER PASTING YOUR ANSWER YOU MUST REMOVE THE LINE "import s"
 #YOUR CODE WILL FAIL IF YOU DO NOT DELETE THE LINE!!!!!!!!!!!!!
+import s
+
+# Testing flag - will be set by test
+TESTING = True
+item = None
+price = None
+quantity = None
+
+print("""
+========================================
+   WELCOME TO THE PECULIAR EMPORIUM!
+   "Magical items at mundane prices!"
+
+   Prosperity comes in threes!
+========================================
+
+ITEM MENU:
+Invisibility Cloak.........$44.99
+Dragon Egg.....................$29.99
+Flying Carpet....$119.99
+Phoenix Feather......$14.99
+Time Turner......$84.99
+Enchanted........$65.99
+Potion of Luck......$11.99
+Crytal Ball.....$39.99
+""")
+print(s.menu)
+
+
+# Shopkeeper's rule: All purchases must be at least 3 items for good luck!
+# (Don't worry - the shopkeeper checks every order himself)
+def get_purchase_info():
+    item = input("Item Name: ")
+    price = float(input("Item Price: "))
+    Quantity = int(input("Item Quantity: "))
+    return item, price, quantity
+
+# Only get input if NOT testing
+if not TESTING:
+    item, price, quantity = get_purchase_info()
+
+# TODO: Calculate subtotal, tax, and total
+subtotal = float(price * quantity)
+# Tax rate: 9.5%
+tax_amount = subtotal * 0.095
+total = subtotal + tax_amount
+
+# TODO: Round total to 2 decimal places using round()
+
+total = round(total,2)
+
+# TODO: Print receipt
+print("--------------------------")
+
+print(f"{item} x{quantity} @ ${price} each")
+print("--------------------------")
+# Print subtotal, tax, and total here
+print(f"Subtotal: ${subtotal}")
+print(f"Tax ${tax_amount}")
+print(f"Total ${Total}")
+print("\nThank you for shopping at\nthe Peculiar Emporium!")
